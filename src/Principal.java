3baceceba296cc1cdb21e5bfcfd419c7351d968c
Bruno <bruno.luis.999@hotmail.com>
2023-09-27 17:16:45 -0300

    Encapsulamento e GETTERS E SETTERS

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,13 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
 
         Filme filme1 = new Filme();
-        filme1.anoDeLancamento = 1998;
-        filme1.nome = "Revolução dos bichos";
-        filme1.duracaoMinutos = 101;
-        filme1.incluidoNoPlano = true;
+        filme1.setAnoDeLancamento(1978);
+        filme1.setNome("Revolução dos bichos");
+        filme1.setDuracaoMinutos(105);
+        filme1.setIncluidoNoPlano(true);
 
 
         // chamada do método
@@ -15,6 +17,10 @@ public class Principal {
         filme1.avalia(5.6);
         System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
         System.out.println("Média das avaliações: " + filme1.pegaMedia());
+        System.out.println("Nome do filme: " + filme1.getNome());
+        System.out.println("Ano de lançamento: " + filme1.getAnoDeLancamento());
+        System.out.println("Duração do filme: " + filme1.getDuracaoMinutos());
+        System.out.println("O filme está incluido no plano: " + filme1.isIncluidoNoPlano());
 
     }
 }
